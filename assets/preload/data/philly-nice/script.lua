dd855091002a40bd1eefc3dd98b35f9dc46139a1
Alguien126 <97417238+Alguien126@users.noreply.github.com>
2022-01-10 16:00:26 -0300

    Add files via upload

---
+++ assets/preload/data/philly-nice/script.lua
@@ -0,0 +1,9 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'bf-os'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'eledeath'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd'); --put in mods/music/
+	--setPropertyFromClass('Note', 'texture', 'ELE_NOTE_assets');
+	--setPropertyFromClass('NoteSplash', 'texture', 'ELEnoteSplashes');
+end
+
